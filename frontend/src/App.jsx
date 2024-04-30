cc2164311df3f17de0aec8525510c5bd0c559450
Hamza Ilyas <Hamzafullstackdev1@gmail.com>
2024-05-01 00:15:59 +0500

    Install And Configure Plugin Flowbite-React

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
-import { useState } from 'react'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-       
+      <BrowserRouter>
+        <Routes>
+          <Route path={"/"} element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

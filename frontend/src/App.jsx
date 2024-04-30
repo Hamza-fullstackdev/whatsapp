8a79fbfb886fda58c4b37101a58fb315ecf5ec61
Hamza Ilyas <Hamzafullstackdev1@gmail.com>
2024-05-01 00:19:02 +0500

    Create Starter Folder Structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path={"/"} element={<Home />} />
+          <Route path={'/signup'} element={<Signup/>}/>
+          <Route path={'/login'} element={<Login/>}/>
         </Routes>
       </BrowserRouter>
     </>

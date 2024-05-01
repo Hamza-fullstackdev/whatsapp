6760bcc9c6f362ff89b45bcfa3bc8a6e7414eb8d
Hamza Ilyas <Hamzafullstackdev1@gmail.com>
2024-05-01 16:32:47 +0500

    Complete Login Page Functionality

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,55 @@
-import React from "react";
-import { Button, Label, Modal, TextInput } from "flowbite-react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import {
+  Alert,
+  Button,
+  Label,
+  Modal,
+  Spinner,
+  TextInput,
+} from "flowbite-react";
+import { Link, useNavigate } from "react-router-dom";
+import { HiInformationCircle } from "react-icons/hi";
 
 const Login = () => {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleData = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+  console.log(formData);
+  const handleFormData = async (e) => {
+    e.preventDefault();
+    setError(false);
+    setLoading(true);
+    try {
+      const res = await fetch("/api/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      setLoading(false);
+      if (data.statusCode === 201) {
+        navigate("/");
+      }
+      if (data.statusCode === 400) {
+        setError(true);
+        setErrorMessage(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
       <div
@@ -14,8 +61,13 @@ const Login = () => {
         </div>
       </div>
       <Modal show popup size={"md"}>
+        {error && (
+          <Alert color='failure' icon={HiInformationCircle}>
+            <span className='font-medium'>Oops!</span> {errorMessage}
+          </Alert>
+        )}
         <Modal.Body>
-          <form>
+          <form onSubmit={handleFormData}>
             <div className='p-2 mt-3'>
               <h3 className='text-xl font-bold text-gray-900 dark:text-white'>
                 Login To Your Whatsapp Account
@@ -26,11 +78,18 @@ const Login = () => {
                   placeholder='Enter your phone number'
                   className='mt-1'
                   type='tel'
+                  id='phone'
+                  onChange={handleData}
                 />
               </div>
               <div className='mt-2'>
                 <Label>Password</Label>
-                <TextInput placeholder='Enter your password' className='mt-1' />
+                <TextInput
+                  placeholder='Enter your password'
+                  className='mt-1'
+                  id='password'
+                  onChange={handleData}
+                />
               </div>
               <div className='mt-3'>
                 <Link
@@ -42,13 +101,23 @@ const Login = () => {
               </div>
               <div className='mt-3'>
                 <div className='flex-1'>
-                  <Button
-                    style={{ background: "#51A985" }}
-                    className='w-full'
-                    type='submit'
-                  >
-                    Login Now
-                  </Button>
+                  {loading ? (
+                    <Button
+                      style={{ background: "#51A985" }}
+                      className='w-full'
+                    >
+                      <Spinner aria-label='Spinner button example' size='sm' />
+                      <span className='pl-3'>Loading...</span>
+                    </Button>
+                  ) : (
+                    <Button
+                      style={{ background: "#51A985" }}
+                      className='w-full'
+                      type='submit'
+                    >
+                      Login
+                    </Button>
+                  )}
                 </div>
               </div>
             </div>

9e11bde89e88b6797134b79942bb67a71cce4fce
Hamza Ilyas <Hamzafullstackdev1@gmail.com>
2024-05-01 08:03:42 +0500

    Create Error Handler Middlewares

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,30 @@
 import express from "express";
-import dotenv from 'dotenv'
-import cors from 'cors';
+import dotenv from "dotenv";
+import cors from "cors";
 import { config } from "./utils/config.util.js";
 
 dotenv.config();
 const app = express();
-const PORT= config.port || 5000;
+const PORT = config.port || 5000;
 
 app.use(express.json());
-app.use(cors({
-  origin: 'http://localhost:5173/',
-  credentials: true,
-}))
+app.use(
+  cors({
+    origin: "http://localhost:5173/",
+    credentials: true,
+  })
+);
+
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
+});
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).send({
+    success: false,
+    statusCode,
+    message,
+  });
 });

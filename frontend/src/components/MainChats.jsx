775dcd2722b53f61dda02b1559ceb59f3dd088ca
Hamza Ilyas <Hamzafullstackdev1@gmail.com>
2024-05-02 00:33:26 +0500

    Welcome Page Design Completed

--- frontend/src/components/MainChats.jsx
+++ frontend/src/components/MainChats.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const MainChats = () => {
   return (
-    <div>MainChats</div>
+    <div className='w-full flex justify-center items-center' style={{height: '100vh', background: 'rgb(227 232 236 / 15%)'}}>
+      <div className='text-center'>
+        <h1 style={{fontSize: '35px' ,color:'#51A985'}}>WhatsApp Web</h1>
+        <p>Hi Hamza, Welcome From The Developer Of This Chat Application</p>
+      </div>
+    </div>
   )
 }
 
--- frontend/src/components/MainSidebar.jsx
+++ frontend/src/components/MainSidebar.jsx
@@ -6,9 +6,9 @@ import { CiSearch } from "react-icons/ci";
 import { Link } from "react-router-dom";
 
 const MainSidebar = () => {
-  const array=[1,2,3,4,5,6,7];
+  const array=[1,2,3,4,5,6,7,8,8,8,8,8,12,1,1,1,1,1,1,1,1,1,1,1];
   return (
-    <div style={{ width: "320px", height: "100vh",borderRight: '1px solid #80808026' }}>
+    <div style={{minWidth: "320px", height: "100vh",borderRight: '1px solid #80808026', overflow: 'auto' }}>
       <div
         className='flex flex-row items-center justify-between'
         style={{ background: "#E3E8EC", padding: "8px 15px" }}
@@ -34,6 +34,7 @@ const MainSidebar = () => {
       </div>
       <div>
         <TextInput
+        className="py-1"
           icon={CiSearch}
           placeholder='Search or start a new chat'
           style={{ borderRadius: 0, background: "white", border: "none" }}
@@ -59,7 +60,7 @@ const MainSidebar = () => {
               <p className='text-sm'>Hamza is a good boy</p>
             </div>
             <div className='w-fit'>
-              <span className='text-sm'>12:40</span>
+              <span className='text-sm' style={{color: '#0000008a'}}>12:40</span>
             </div>
           </div>
           ))
